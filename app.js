f3247cd1b3ea400a1a85ce0519d65724d2ac7f70
Fady Sadakah <fadysadaqah@gmail.com>
2019-11-23 14:08:20 +0200

    homeeeeee

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ app.use(express.static('public'));
 app.get("/item",function(req,res){
     res.render('item-div.ejs');
 })
+app.get("/",function(req,res){
+    res.render('home.ejs');
+})
+app.get("/m",function(req,res){
+    res.render('mobile/home.ejs');
+})
 app.listen(5000)
